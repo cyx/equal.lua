3c710c5733a1456e8cea33f20dfc16cdf599795e
Cyril David <cyx@cyx.is>
2014-04-17 19:04:21 -0700

    Add false cases in test

--- deep_equal_test.lua
+++ deep_equal_test.lua
@@ -7,16 +7,22 @@ assert(deep_equal(true, true))
 assert(deep_equal(false, false))
 assert(deep_equal("1", "1"))
 
+assert(false == deep_equal(1, 2))
+
 -- case 2: list of primitive types
 assert(deep_equal({1, 2}, {1, 2}))
+assert(false == deep_equal({1, 2}, {2, 1}))
 
 -- case 3: dictionary of primitive types
 assert(deep_equal({a = 1, b = 2}, {a = 1, b = 2}))
+assert(false == deep_equal({a = 2, b = 2}, {a = 1, b = 2}))
 
 -- case 4: deeper list
 assert(deep_equal({{1, 2}, {3, 4}}, {{1, 2}, {3, 4}}))
+assert(false == deep_equal({{1, 2}, {3, 3}}, {{1, 2}, {3, 4}}))
 
 -- case 5: deeper dictionary
 assert(deep_equal({a = {b = 2, c = 3}}, {a = {b = 2, c = 3}}))
+assert(false == deep_equal({a = {b = 2, c = 4}}, {a = {b = 2, c = 3}}))
 
 print("All tests passed.")
